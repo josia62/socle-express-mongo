d384d453bb2d044967d60b7b8958a38014200868
josia62 <rafidinavalonajosia62@gmail.com>
2024-09-17 16:03:41 +0300

    ok socket

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import "reflect-metadata";
 
 import App from "./app";
+import { ws } from "./service/middleware/socket";
 
 const bootstrapAsync = async () => {
   try {
-    await App.init();
+    const server = await App.init();
+
+    // Initialisation socket à supprimer si non utilisé
+    ws.init(server);
   } catch (error) {
     console.log(error);
   }
